8cf5b7cbfa1048c7faf806e36950c169f2706a63
PurpleValentina <33701933+PurpleValentina@users.noreply.github.com>
2017-12-28 15:34:41 +0200

    Add files via upload

---
+++ scriptjs.js
@@ -0,0 +1,21 @@
+var review = document.getElementById("info");
+var btn = document.getElementById("btn");
+btn.addEventListener("click",function(){
+var myRequest= new XMLHttpRequest();
+myRequest.open('GET','script.json');
+myRequest.onload=function(){
+    var myData=JSON.parse(myRequest.responseText);
+    renderHTML(myData);
+};
+myRequest.send();
+
+});
+function renderHTML(data){
+var htmlString="";
+for(i=0;i<data.length;i++) {
+htmlString += "<p>"+data[i].name+ "is a"+data[i].event+".</p>";
+
+}
+
+
+}
